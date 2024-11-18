4c56f0228c50f191b84d9a16700ae7471881d783
Enzo Dutra <enzorei111x@gmail.com>
2024-11-18 16:42:49 -0300

    alteração base projeto

---
+++ README.md
@@ -0,0 +1,6 @@
+TechLead -  Enzo Dutra
+Dev's e suas funções:
+Ithallo Emanuel - Criar a função de soma
+Tarik Kassem - Criar a função de subtração
+Vitor Cruz - Criar a função de multiplicação
+Matheus Schneider - Criar a função de divisão
--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-// TechLead -  Enzo Dutra
-// Dev's e suas funções:
-// Ithallo Emanuel - Criar a função de soma
-// Tarik Kassem - Criar a função de subtração
-// Vitor Cruz - Criar a função de multiplicação
-// Matheus Schneider - Criar a função de divisão
+// Projeto funções básicas de cálculo
