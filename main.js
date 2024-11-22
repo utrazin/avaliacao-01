9cd39c829b3a8ef29ba44fa7c81bae2a3a01411c
Enzo Dutra <101825981+utrazin@users.noreply.github.com>
2024-11-21 21:02:54 -0300

    Revert "Adiciona funcionalidade de divisão"

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
 // Projeto funções básicas de cálculo
-
-// Função de soma - Ithallo Emanuel 
-function soma(a,b) {
-    return a + b;
-}
-
-// Função de multiplicação - Vitor Cruz
-function multiplicacao(a,b) {
-    return a * b;
-}
-
-// Função de divisão - Matheus Schneider
-function divisao(a,b) {
-    return a / b;
-}
